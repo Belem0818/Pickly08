a961d6f0bc744c3b0b9179609abd587b38bff79a
Belem0818 <60374047+Belem0818@users.noreply.github.com>
2020-06-05 15:20:19 -0500

    Add files via upload

---
+++ PRACTICA 1/PRACTICA 1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+	printf("\n Hola mundo, Bienvenidos a el Laboratorio de programacion estructurada");_flushall;
+	
+	system ("pause");
+	return 0;
+}
